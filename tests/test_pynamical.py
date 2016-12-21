f6e889b969f95e270b9fd5caf91dba2af185d3b7
gboeing <gboeing@berkeley.edu>
2016-12-21 10:33:21 -0800

    added import tests

--- tests/test_pynamical.py
+++ tests/test_pynamical.py
@@ -4,6 +4,6 @@ pynamical tests
 """
 
 def test_imports():
-    #from pynamical import simulate, logistic_map, cubic_map
-    #from pynamical import bifurcation_plot, cobweb_plot, phase_diagram, phase_diagram_3d
-    pass
+    from pynamical import simulate, logistic_map, cubic_map
+    from pynamical import bifurcation_plot, cobweb_plot, phase_diagram, phase_diagram_3d
+    
