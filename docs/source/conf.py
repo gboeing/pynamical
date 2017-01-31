5d03ea0199c3c0d77988fa7ceb866c7c5a68e125
gboeing <gboeing@berkeley.edu>
2017-01-31 13:15:52 -0800

    remove github pages sphinx extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,8 +39,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
