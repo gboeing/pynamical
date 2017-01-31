577a24991bb32b5640ac066927f7465b17b10207
gboeing <gboeing@berkeley.edu>
2017-01-31 13:13:55 -0800

    change sphinx napoleon extension source

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinxcontrib.napoleon',
+    'sphinx.ext.napoleon',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
