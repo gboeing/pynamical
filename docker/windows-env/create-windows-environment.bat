3b44d095ef83562748ba6abe535a282629ff7ca7
gboeing <g.boeing@northeastern.edu>
2019-03-11 15:58:48 -0400

    update docker

--- docker/windows-env/create-windows-environment.bat
+++ docker/windows-env/create-windows-environment.bat
@@ -1,7 +1,7 @@
-CALL conda update conda -n base --yes
+CALL conda update --strict-channel-priority --yes -n base conda
 CALL conda deactivate
 CALL conda env remove -n pyn --yes
-CALL conda create -n pyn -c conda-forge --strict-channel-priority --file "../../requirements.txt" --file "../../requirements-dev.txt" --file "requirements-more.txt" --yes
+CALL conda create -n pyn -c conda-forge --strict-channel-priority --file "../../requirements.txt" --file "../../requirements-dev.txt" --file "../requirements-more.txt" --yes
 CALL conda activate pyn
 CALL python -m ipykernel install --user
 CALL conda clean --all --yes
