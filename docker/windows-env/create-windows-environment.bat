68a899a67ee76f301128ba9c7df90407eb2ffe8f
gboeing <g.boeing@northeastern.edu>
2019-03-13 12:50:21 -0400

    update env

--- docker/windows-env/create-windows-environment.bat
+++ docker/windows-env/create-windows-environment.bat
@@ -3,6 +3,9 @@ CALL conda deactivate
 CALL conda env remove -n pyn --yes
 CALL conda create -n pyn -c conda-forge --strict-channel-priority --file "../../requirements.txt" --file "../../requirements-dev.txt" --file "../requirements-more.txt" --yes
 CALL conda activate pyn
-CALL python -m ipykernel install --user
+CALL python -m ipykernel install --user --name pyn --display-name "Python (pyn)"
 CALL conda clean --all --yes
 CALL conda env export > environment-windows.yml
+CALL conda list
+CALL jupyter kernelspec list
+CALL python -c "import pynamical; print(pynamical.__version__)"
